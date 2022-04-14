4f74af264103165ad3fb5083c45f6f589ff7883c
common khadka <commonkhadka@gmail.com>
2022-04-14 21:35:09 +0545

    Dashboard viewe added

--- ECommerceSample/Controllers/DashboardController.cs
+++ ECommerceSample/Controllers/DashboardController.cs
@@ -1,14 +1,37 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ECommerce.Repository;
+using ECommerceSample.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceSample.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ProductRepositoryInterface _productRepo;
+        private readonly BrandRepositoryInterface _brandRepo;
+        private readonly CategoryRepositoryInterface _categoryRepo;
+        private readonly TagRepositoryInterface _tagRepo;
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ProductRepositoryInterface productRepo, BrandRepositoryInterface brandRepo,
+            CategoryRepositoryInterface categoryRepo, TagRepositoryInterface tagRepo, ILogger<DashboardController> logger)
         {
-            return View();
+            _productRepo = productRepo;
+            _brandRepo = brandRepo;
+            _categoryRepo = categoryRepo;
+            _tagRepo = tagRepo;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(DashboardViewModel model)
+        {
+            model.TotalProducts = await _productRepo.GetQueryable().CountAsync();
+            model.TotalCategories = await _categoryRepo.GetQueryable().CountAsync();
+            model.TotalBrands = await _brandRepo.GetQueryable().CountAsync();
+            model.TotalTags = await _tagRepo.GetQueryable().CountAsync();
+            return View(model);
         }
     }
 }
---
+++ ECommerceSample/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceSample.Models
+{
+    public class DashboardViewModel
+    {
+        public long TotalProducts { get; set; }
+        public long TotalBrands { get; set; }
+        public long TotalTags { get; set; }
+        public long TotalCategories { get; set; }
+
+    }
+}
